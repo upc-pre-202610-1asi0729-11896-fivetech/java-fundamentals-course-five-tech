26dcb1e97acb9cb46461314b1f0f5e4ce30addf3
Frase <ytcarniy@gmail.com>
2026-06-02 13:33:25 -0500

    feature: add code example module 2

---
+++ completed-examples/module-02/Main.java
@@ -0,0 +1,14 @@
+public class Main
+{
+	public static void main(String[] args) {
+		String nombre = "Juan";
+		int edad = 15;
+		double promedio = 17.5;
+		
+		System.out.println(nombre);
+		System.out.println(edad);
+		System.out.println(promedio);
+		
+		System.out.println("Hola " + nombre);
+	}
+}

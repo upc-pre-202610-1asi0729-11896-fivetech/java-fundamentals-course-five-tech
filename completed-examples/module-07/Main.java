1f50eec405b6f10cdd752677ebac142788501a69
AgoxX61 <134884913+AgoxX61@users.noreply.github.com>
2026-06-14 18:59:19 -0500

    Update Main.java

---
+++ completed-examples/module-07/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println(" MODULO 7: CLASES Y OBJETOS ");
+        System.out.println("=================================");
+
+        Estudiante estudianteEjemplo =
+                new Estudiante("Luis", 16, 14, 18);
+
+        estudianteEjemplo.mostrarDatos();
+    }
+}
